19a51f36897b9c8ba1b5c540f26976cb54ea945b
Pulak <roy2004pulok@gmail.com>
2025-07-18 22:57:16 -0600

    Sp.cpp added

---
+++ sp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum;
+
+  cout<<"Enter a 1st number:";
+  cin>>a;
+  cout<<"Enter a 2nd number:";
+  cin>>b;
+
+  sum = a+b;
+
+  cout<<"sum :"<<sum<<endl;
+
+ return 0;
+}
